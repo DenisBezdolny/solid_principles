9ed41a7e6850a0fe9896930f993c3f70b38e2889
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-03-27 16:21:38 +0300

    Add new implementation classes

---
+++ SOLID/SOLID/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    public class Item : IActionOfItem
+    {
+        private string _name;
+        public int Hitpoints { get; }
+
+        public int Weight { get; }
+
+        public int Healpoints { get; }
+
+        public int Damagepoints { get; }
+
+        public Item(string _name)
+        {
+            _name = "Karl";
+        }
+
+        public string Name => _name;
+    }
+}
---
+++ SOLID/SOLID/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    public class Weapon : IActionOfItem
+    {
+        private string _name;
+        public int Hitpoints {  get; }
+
+        public int Weight { get; }
+
+        public int Healpoints { get; }
+
+        public int Damagepoints { get; }
+
+        public Weapon (string _name)
+        {
+            _name = "Karl";
+        }
+
+        public string Name => _name;
+    }
+}
